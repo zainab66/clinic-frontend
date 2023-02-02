0677cc06fdc12109340bd29cf680b27e88bf038c
zainab66 <zainab66r@gmail.com>
2023-02-02 16:50:54 -0500

    Fixing baseurl

--- src/actions/assistantAction.js
+++ src/actions/assistantAction.js
@@ -1,10 +1,13 @@
 import Axios from 'axios';
 import { toast } from 'react-toastify';
 
+const baseURL = 'https://xi-team-api.onrender.com/api';
+// || 'http://localhost:3001/api';
+
 // Add User
 export const addUser = async (email, fullName, role, createdBy) => {
   const response = await Axios.post(
-    'http://localhost:3001/api/assistant/addUser',
+    `${baseURL}/assistant/addUser`,
     {
       email,
       fullName,
@@ -28,53 +31,36 @@ export const addUser = async (email, fullName, role, createdBy) => {
 };
 
 export const getUsersList = async () => {
-  const response = await Axios.get(
-    'http://localhost:3001/api/assistant/getUsers',
-    {
-      headers: {
-        Authorization: `Bearer ${
-          JSON.parse(localStorage.getItem('user')).token
-        }`,
-      },
-    }
-  );
+  const response = await Axios.get(`${baseURL}/assistant/getUsers`, {
+    headers: {
+      Authorization: `Bearer ${JSON.parse(localStorage.getItem('user')).token}`,
+    },
+  });
   return response.data;
 };
 
 export const activateUser = async (token) => {
-  const response = await Axios.post(
-    'http://localhost:3001/api/assistant/email-activate',
-    { token }
-  );
+  const response = await Axios.post(`${baseURL}/assistant/email-activate`, {
+    token,
+  });
 
   return response.data;
 };
 
 export const deleteUserInfo = async (userId) => {
-  const response = await Axios.delete(
-    `https://xi-team-api.onrender.com/api/assistant/${userId}`,
-    {
-      headers: {
-        Authorization: `Bearer ${
-          JSON.parse(localStorage.getItem('user')).token
-        }`,
-      },
-    }
-  );
+  const response = await Axios.delete(`${baseURL}/assistant/${userId}`, {
+    headers: {
+      Authorization: `Bearer ${JSON.parse(localStorage.getItem('user')).token}`,
+    },
+  });
   return response.data;
 };
 
 export const editUser = async (user) => {
-  const response = await Axios.put(
-    `https://xi-team-api.onrender.com/api/assistant/${user._id}`,
-    user,
-    {
-      headers: {
-        Authorization: `Bearer ${
-          JSON.parse(localStorage.getItem('user')).token
-        }`,
-      },
-    }
-  );
+  const response = await Axios.put(`${baseURL}/assistant/${user._id}`, user, {
+    headers: {
+      Authorization: `Bearer ${JSON.parse(localStorage.getItem('user')).token}`,
+    },
+  });
   return response.data;
 };
